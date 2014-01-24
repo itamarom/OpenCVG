12029a46e3ed5820dced455df88288afb46b9176
idoweins <ido.weins@gmail.com>
2014-01-23 21:30:25 -0800

    Added support for more types, test now tries creating a form for Draw in image.

--- OpenCVG/Form1.cs
+++ OpenCVG/Form1.cs
@@ -6,17 +6,12 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Emgu.CV.Structure;
+using Emgu.CV;
+using System.Reflection;
 
 namespace OpenCVG
 {
-    static class StringJoinExtendor
-    {
-        public static String Join(this string self, params object[] objs)
-        {
-            return String.Join(self, objs);
-        }
-    }
-
     public partial class Form1 : Form
     {
         public Form1()
@@ -26,8 +21,66 @@ namespace OpenCVG
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            FuncForm ff = new FuncForm(this);
-            ff.BuildGui(new Type[] { typeof(int), typeof(int) });
+
+            Type t = typeof(Image<Bgr, Byte>);
+
+
+            MethodInfo[] ms = GetMethodsByName(t, "Draw");
+
+            foreach (var m in ms) {
+                try
+                {
+                    Type[] ta = m.GetParameters().Select(p => p.ParameterType).ToArray();
+                    FuncForm ff = new FuncForm(this, ta);
+                    Type bla = ta[1];
+                    ff.BuildGui();
+
+                    this.label1.Text += "\nSuccess!!!";
+                    break;
+                }
+                catch (TypeGUINotFoundError ex)
+                {
+                    this.label1.Text += "\nNo type:" + ex.t.FullName;
+                }
+                catch (Exception yute)
+                {
+                    this.label1.Text += "\nFUCK:" + yute.Message;
+               
+                }
+            }
+            
+        }
+
+        private MethodInfo[] GetMethodsByName(Type t, string name)
+        {
+            return t.GetMethods().Where(m => m.Name == name).ToArray();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+
+            using (Image<Bgr, Byte> img = new Image<Bgr, byte>(400, 200, new Bgr(255, 0, 0)))
+            {
+                //Create the font
+                MCvFont f = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_COMPLEX, 1.0, 1.0);
+
+                //Draw "Hello, world." on the image using the specific font
+                img.Draw("Hello, world", ref f, new Point(10, 80), new Bgr(0, 255, 0));
+                   
+                this.BackgroundImage = img.ToBitmap();
+            }
         }
     }
+
+
+    static class StringJoinExtendor
+    {
+        public static String Join(this string self, params object[] objs)
+        {
+            return String.Join(self, objs);
+        }
+    }
+
 }
--- OpenCVG/TypeGUI.cs
+++ OpenCVG/TypeGUI.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Drawing;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 
 namespace OpenCVG
 {
@@ -12,7 +14,7 @@ namespace OpenCVG
 
     class TypeGUINotFoundError : Exception
     {
-        Type t { get; set; }
+        public Type t { get; set; }
 
         public TypeGUINotFoundError(Type t)
         {
@@ -35,14 +37,15 @@ namespace OpenCVG
     class FuncForm
     {
         public static Dictionary<Type, TypeGUI> guis = new Dictionary<Type, TypeGUI>();
-        
+
         static FuncForm()
         {
             guis.Add(typeof(int), new TypeGUI(
-                delegate(Control container, object tag, Point location,object initialValue)
+                delegate(Control container, object tag, Point location, object initialValue)
                 {
                     NumericUpDown input = new NumericUpDown();
-                    
+
+
                     if (initialValue != null)
                         input.Value = (int)initialValue;
 
@@ -53,13 +56,150 @@ namespace OpenCVG
                     container.Controls.Add(input);
 
                     return input.Size;
-                    
+
 
                 }, delegate(Control container, object tag)
                 {
                     NumericUpDown input = (NumericUpDown)ControlByTag(container, tag);
                     return input.Value;
                 }));
+
+            guis.Add(typeof(MCvFont), new TypeGUI(
+                delegate(Control container, object tag, Point location, object initialValue)
+                {
+                    TextBox hScaleTb = new TextBox();
+                    hScaleTb.Location = location;
+                    //hScaleTb.Text = initialValue.ToString();
+                    hScaleTb.Tag = SubTag(tag, 0);
+
+                    location.Y += hScaleTb.Height + 20;
+
+                    TextBox vScaleTb = new TextBox();
+                    vScaleTb.Location = location;
+                    //vScaleTb.Text = initialValue.ToString();
+                    vScaleTb.Tag = SubTag(tag, 1);
+
+                    container.Controls.Add(hScaleTb);
+                    container.Controls.Add(vScaleTb);
+
+                    return hScaleTb.Size + vScaleTb.Size + new Size(0, 20);
+                },
+                delegate(Control container, object tag)
+                {
+                    double h = 0, v = 0;
+
+                    try
+                    {
+                        h = double.Parse(((TextBox)ControlByTag(container, tag, 0)).Text);
+                        v = double.Parse(((TextBox)ControlByTag(container, tag, 1)).Text);
+
+                    }
+                    catch (Exception e)
+                    {
+                        //TODO: throw excpetion.
+                    }
+
+                    return new MCvFont(FONT.CV_FONT_HERSHEY_COMPLEX, h, v);
+                }
+
+                ));
+
+            guis.Add(typeof(double), new TypeGUI(
+           delegate(Control container, object tag, Point location, object initialValue)
+           {
+               if (initialValue == null)
+                   initialValue = 0;
+
+               TextBox input = new TextBox();
+               input.Location = location;
+               input.Text = initialValue.ToString();
+               input.Tag = tag;
+
+               return input.Size;
+           },
+           delegate(Control container, object tag)
+           {
+               double x = 0;
+
+               try
+               {
+                   x = double.Parse(((TextBox)ControlByTag(container, tag)).Text);
+
+               }
+               catch (Exception e)
+               {
+                   //TODO: throw excpetion.
+               }
+
+               return x;
+           }));
+            guis.Add(typeof(string), new TypeGUI(
+delegate(Control container, object tag, Point location, object initialValue)
+{
+    if (initialValue == null)
+        initialValue = "";
+
+    TextBox input = new TextBox();
+    input.Location = location;
+    input.Text = initialValue.ToString();
+    input.Tag = tag;
+
+    return input.Size;
+},
+delegate(Control container, object tag)
+{
+    return ((TextBox)ControlByTag(container, tag)).Text;
+}));
+
+            guis.Add(typeof(Point), new TypeGUI(
+               delegate(Control container, object tag, Point location, object initialValue)
+               {
+                   TextBox x = new TextBox();
+                   x.Location = location;
+                   //hScaleTb.Text = initialValue.ToString();
+                   x.Tag = SubTag(tag, 0);
+
+                   location.Y += x.Height + 20;
+
+                   TextBox y = new TextBox();
+                   y.Location = location;
+                   //vScaleTb.Text = initialValue.ToString();
+                   y.Tag = SubTag(tag, 1);
+
+                   container.Controls.Add(x);
+                   container.Controls.Add(y);
+
+                   return x.Size + y.Size + new Size(0, 20);
+               },
+               delegate(Control container, object tag)
+               {
+
+                   int x = 0, y = 0;
+
+                   try
+                   {
+                       x = int.Parse(((TextBox)ControlByTag(container, tag, 0)).Text);
+                       y = int.Parse(((TextBox)ControlByTag(container, tag, 1)).Text);
+
+                   }
+                   catch (Exception e)
+                   {
+                       //TODO: throw excpetion.
+                   }
+
+                   return new Point(x, y);
+               }));
+
+        }
+
+        static String SubTag(object tag, object subtag)
+        {
+            return tag.ToString() + "_sub_" + subtag.ToString();
+        }
+
+        static Control ControlByTag(Control container, object tag, object subtag)
+        {
+            return ControlByTag(container, SubTag(tag, subtag));
         }
 
         static Control ControlByTag(Control container, object tag)
@@ -75,31 +215,60 @@ namespace OpenCVG
             return null;
         }
 
-        Control container;
 
-        public FuncForm(Control container)
+        private Type RemoveRefIfExist(Type t)
         {
-            this.container = container;
+            if (t.IsByRef)
+                return t.Module.GetType(t.FullName.Replace("&", ""));
+
+            return t;
         }
 
-        public void BuildGui(Type[] types)
+
+        Control container;
+        Type[] types;
+
+        public FuncForm(Control container, Type[] types)
+        {
+            this.container = container;
+            this.types = types;
+        }
+        public void BuildGui()
         {
             Point location = new Point();
             const int MARGIN = 20;
 
             for (int i = 0; i < types.Length; i++)
             {
-                if (guis.ContainsKey(types[i]))
+                Type t = RemoveRefIfExist(types[i]);
+
+                if (guis.ContainsKey(t))
                 {
-                    Size s = guis[types[i]].AddControls(this.container, i, location, null);
+
+                    Size s = guis[t].AddControls(this.container, i, location, null);
                     location.Y += s.Height;
                     location.Y += MARGIN;
                 }
                 else
                 {
-                    throw new TypeGUINotFoundError(types[i]);
+                    throw new TypeGUINotFoundError(t);
                 }
             }
         }
+
+        public object[] GetValues()
+        {
+            object[] result = new object[types.Length];
+
+            for (int i = 0; i < types.Length; i++)
+            {
+                Type t = RemoveRefIfExist(types[i]);
+
+                result[i] = guis[t].GetValue(this.container, i);
+            }
+
+            return result;
+        }
+
     }
 }
