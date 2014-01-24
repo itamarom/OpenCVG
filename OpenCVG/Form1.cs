facee11ea42900d03c00cdb80924381a4602a210
idoweins <ido.weins@gmail.com>
2014-01-23 19:51:11 -0800

    First commit.

---
+++ OpenCVG/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OpenCVG
+{
+    static class StringJoinExtendor
+    {
+        public static String Join(this string self, params object[] objs)
+        {
+            return String.Join(self, objs);
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FuncForm ff = new FuncForm(this);
+            ff.BuildGui(new Type[] { typeof(int), typeof(int) });
+        }
+    }
+}
---
+++ OpenCVG/TypeGUI.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace OpenCVG
+{
+    delegate Size AddControlsFunc(Control container, object tag, Point location, object initialValue);
+    delegate object GetValueFunc(Control container, object tag);
+
+    class TypeGUINotFoundError : Exception
+    {
+        Type t { get; set; }
+
+        public TypeGUINotFoundError(Type t)
+        {
+            this.t = t;
+        }
+    }
+
+    class TypeGUI
+    {
+        public AddControlsFunc AddControls { get; set; }
+        public GetValueFunc GetValue { get; set; }
+
+        public TypeGUI(AddControlsFunc a, GetValueFunc b)
+        {
+            this.AddControls = a;
+            this.GetValue = b;
+        }
+    }
+
+    class FuncForm
+    {
+        public static Dictionary<Type, TypeGUI> guis = new Dictionary<Type, TypeGUI>();
+        
+        static FuncForm()
+        {
+            guis.Add(typeof(int), new TypeGUI(
+                delegate(Control container, object tag, Point location,object initialValue)
+                {
+                    NumericUpDown input = new NumericUpDown();
+                    
+                    if (initialValue != null)
+                        input.Value = (int)initialValue;
+
+                    input.Location = location;
+                    input.Tag = tag;
+                    input.Minimum = int.MinValue;
+                    input.Maximum = int.MaxValue;
+                    container.Controls.Add(input);
+
+                    return input.Size;
+                    
+
+                }, delegate(Control container, object tag)
+                {
+                    NumericUpDown input = (NumericUpDown)ControlByTag(container, tag);
+                    return input.Value;
+                }));
+        }
+
+        static Control ControlByTag(Control container, object tag)
+        {
+            for (int i = 0; i < container.Controls.Count; i++)
+            {
+                if (container.Controls[i].Tag == tag)
+                {
+                    return container.Controls[i];
+                }
+            }
+
+            return null;
+        }
+
+        Control container;
+
+        public FuncForm(Control container)
+        {
+            this.container = container;
+        }
+
+        public void BuildGui(Type[] types)
+        {
+            Point location = new Point();
+            const int MARGIN = 20;
+
+            for (int i = 0; i < types.Length; i++)
+            {
+                if (guis.ContainsKey(types[i]))
+                {
+                    Size s = guis[types[i]].AddControls(this.container, i, location, null);
+                    location.Y += s.Height;
+                    location.Y += MARGIN;
+                }
+                else
+                {
+                    throw new TypeGUINotFoundError(types[i]);
+                }
+            }
+        }
+    }
+}
